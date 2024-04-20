33b8be99a2b93d03372ece11cc30fec63f6b2c30
Xcilik <149920327+Xcilik@users.noreply.github.com>
2024-04-20 13:26:30 +0700

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -5,12 +5,20 @@ from utils.misc import modules_help
 from utils.filters import command 
 
 
-@Client.on_message(command("cmds") & filters.me)
+@Client.on_message(command(["cmds", "shit"]) & filters.me)
 async def cmds(client: Client, message: Message):
-    x = await client.get_inline_bot_results("Hayday3wFbot", "")
-    for y in x.results:
-        await client.send_inline_bot_result(message.chat.id, x.query_id, y.id, reply_to_message_id=message.id)
-
+    if message.command[0] == "cmds":
+        text = "commands"
+    elif message.command[0] == "shit":
+        text = "shit"    
+    try:
+        x = await client.get_inline_bot_results("Hayday3wFbot", text)
+        for m in x.results:
+            await client.send_inline_bot_result(
+                message.chat.id, x.query_id, m.id, reply_to_message_id=message.id
+            )
+    except Exception as error:
+        await message.edit(str(error))
 
 module = modules_help.add_module("cmds", __file__)
 module.add_command("cmds", "لرؤية الاوامر الخاصة باليوزربوت")
