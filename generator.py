55b5bd20ea41f5a473652c8609682b52c909e7f0
i3wF <hmwdyalharby7566277@gmail.com>
2024-10-25 19:05:13 +0300

    add string_session exporter

---
+++ generator.py
@@ -0,0 +1,21 @@
+from pyrogram import Client
+import asyncio
+
+API_ID = int(input("Please enter your API ID: "))
+API_HASH = input("Please enter your API Hash: ")
+
+
+async def export_session():
+    async with Client(
+        "generator",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        lang_code="ar",
+        device_model="MacBook Pro M1",
+        system_version="14.3.1",
+    ) as app:
+        session_string = await app.export_session_string()
+        app.send_message("me", f"STRING_SESSION: `{session_string}`")
+
+
+asyncio.run(export_session())
