13c199ba90435719ea4ab1ef69399fe5d3ae532e
i3wF <hmwdyalharby7566277@gmail.com>
2024-10-25 19:54:38 +0300

    update reply_to_me.py

--- plugins/reply_to_me.py
+++ plugins/reply_to_me.py
@@ -35,59 +35,59 @@ async def reply_to_me(client: Client, message: Message):
         if user_info.username
         else f"({name})[tg://user?id={user_id}]"
     )
-
-    if message.reply_to_message.from_user.id == client.me.id:
-        response_message = (
-            message.text
-            or message.video
-            or message.sticker
-            or message.animation
-            or message.photo
-            or message.audio
-            or message.voice
-        )
-
-        async def send_response(message_type, file_id=None, text=None):
-            type_translations = {
-                Video: "فيديو",
-                Sticker: "ملصق",
-                Animation: "متحرك",
-                Photo: "صورة",
-                Audio: "صوتي",
-                Voice: "فويس",
-                str: "نص",
-            }
-
-            type_label = type_translations.get(message_type, "نص")
-
-            RTM = (
-                f"\nيوجد شخصاً ما رد عليك {mention}\n"
-                f"اسمه: {name}\n"
-                f"ايديه: {user_id}\n"
-                f"رابط الرسالة : {link}\n"
-                f"نوع الرسالة: {type_label}\n"
-                f"الوقت: {formatted_time}"
+    if message.reply_to_message:
+        if message.reply_to_message.from_user.id == client.me.id:
+            response_message = (
+                message.text
+                or message.video
+                or message.sticker
+                or message.animation
+                or message.photo
+                or message.audio
+                or message.voice
             )
 
-            if message_type is str and text:
-                RTM += f"\nمحتوى الرسالة: {text}"
+            async def send_response(message_type, file_id=None, text=None):
+                type_translations = {
+                    Video: "فيديو",
+                    Sticker: "ملصق",
+                    Animation: "متحرك",
+                    Photo: "صورة",
+                    Audio: "صوتي",
+                    Voice: "فويس",
+                    str: "نص",
+                }
 
-            await client.send_message(chat_id=GID, text=RTM)
+                type_label = type_translations.get(message_type, "نص")
 
-            if file_id:
-                await {
-                    Video: client.send_video,
-                    Sticker: client.send_sticker,
-                    Animation: client.send_animation,
-                    Photo: client.send_photo,
-                    Audio: client.send_audio,
-                    Voice: client.send_voice,
-                }[message_type](chat_id=GID, **{message_type.__name__.lower(): file_id})
+                RTM = (
+                    f"\nيوجد شخصاً ما رد عليك {mention}\n"
+                    f"اسمه: {name}\n"
+                    f"ايديه: {user_id}\n"
+                    f"رابط الرسالة : {link}\n"
+                    f"نوع الرسالة: {type_label}\n"
+                    f"الوقت: {formatted_time}"
+                )
 
-        if isinstance(
-            response_message, (Video, Sticker, Animation, Photo, Audio, Voice)
-        ):
-            await send_response(type(response_message), response_message.file_id)
+                if message_type is str and text:
+                    RTM += f"\nمحتوى الرسالة: {text}"
 
-        elif message.text:
-            await send_response(str, text=message.text)
+                await client.send_message(chat_id=GID, text=RTM)
+
+                if file_id:
+                    await {
+                        Video: client.send_video,
+                        Sticker: client.send_sticker,
+                        Animation: client.send_animation,
+                        Photo: client.send_photo,
+                        Audio: client.send_audio,
+                        Voice: client.send_voice,
+                    }[message_type](chat_id=GID, **{message_type.__name__.lower(): file_id})
+
+            if isinstance(
+                response_message, (Video, Sticker, Animation, Photo, Audio, Voice)
+            ):
+                await send_response(type(response_message), response_message.file_id)
+
+            elif message.text:
+                await send_response(str, text=message.text)
